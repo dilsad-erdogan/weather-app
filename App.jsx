3bbc113822d91c1ba07f23d970f7b3511e26b9ac
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-11-14 21:00:32 +0300

    general start

--- App.jsx
+++ App.jsx
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { Image, SafeAreaView, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import "./global.css";
-import { MagnifyingGlassIcon } from 'react-native-heroicons/outline';
+import { CalendarDaysIcon, MagnifyingGlassIcon } from 'react-native-heroicons/outline';
 import { MapPinIcon } from 'react-native-heroicons/solid';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
+import { ScrollView } from 'react-native-reanimated/lib/typescript/Animated';
+import { debounce } from 'lodash';
 
 export default function App() {
   const [showSearch, setShowSearch] = useState(false);
@@ -11,7 +13,13 @@ export default function App() {
 
   const handleLocation = (loc) => {
     console.log('location: ', loc);
-  }
+  };
+
+  const handleSearch = value => {
+    console.log(value);
+  };
+
+  const handleTextDebounce = useCallback(debounce(handleSearch, 1200), []);
 
   return (
     <View className='flex-1 relative'>
@@ -23,7 +31,7 @@ export default function App() {
         {/* Search Section */}
         <View style={{height: '8%'}} className='mt-14 mx-4 relative z-50'>
           <View className={`flex-row justify-end items-center rounded-full ${showSearch ? 'bg-gray-600' : 'bg-transparent'}`}>
-            {showSearch ? (<TextInput placeholder='Search city' placeholderTextColor={'lightgray'} className='pl-6 h-10 flex-1 text-base text-white' />) : null}
+            {showSearch ? (<TextInput onChange={handleTextDebounce} placeholder='Search city' placeholderTextColor={'lightgray'} className='pl-6 h-10 flex-1 text-base text-white' />) : null}
 
             <TouchableOpacity onPress={() => setShowSearch(!showSearch)} className='bg-gray-500 rounded-full p-3 m-1'>
               <MagnifyingGlassIcon size={25} color={"white"} />
@@ -73,7 +81,74 @@ export default function App() {
           </View>
 
           {/* Other Stats */}
-          <View></View>
+          <View className='flex-row justify-between mx-4'>
+            <View className='flex-row space-x-2 items-center'>
+              <Image source={require('./assets/drizzle.png')} className='h-6 w-6' />
+              <Text className='text-white font-semibold text-base'>22km</Text>
+            </View>
+
+            <View className='flex-row space-x-2 items-center'>
+              <Image source={require('./assets/rain.png')} className='h-6 w-6' />
+              <Text className='text-white font-semibold text-base'>23%</Text>
+            </View>
+
+            <View className='flex-row space-x-2 items-center'>
+              <Image source={require('./assets/clear.png')} className='h-6 w-6' />
+              <Text className='text-white font-semibold text-base'>6:05 AM</Text>
+            </View>
+          </View>
+        </View>
+
+        {/* Forecast for next days */}
+        <View className='mb-2 space-y-3'>
+          <View className='flex-row items-center mx-5 space-x-2'>
+            <CalendarDaysIcon size={22} color="white" />
+            <Text className='text-white text-base'>Daily forecast</Text>
+          </View>
+
+          <ScrollView horizontal contentContainerStyle={{paddingHorizontal: 15}} showsHorizontalScrollIndicator={false}>
+            <View className='flex justify-center items-center w-24 rounded-3xl py-3 space-y-1 mr-4 bg-gray-500'>
+              <Image source={require('./assets/rain.png')} className='h-11 w-11' />
+              <Text className='text-white'>Monday</Text>
+              <Text className='text-white text-xl font-semibold'>13&#176;</Text>
+            </View>
+
+            <View className='flex justify-center items-center w-24 rounded-3xl py-3 space-y-1 mr-4 bg-gray-500'>
+              <Image source={require('./assets/clear.png')} className='h-11 w-11' />
+              <Text className='text-white'>Tuesday</Text>
+              <Text className='text-white text-xl font-semibold'>13&#176;</Text>
+            </View>
+
+            <View className='flex justify-center items-center w-24 rounded-3xl py-3 space-y-1 mr-4 bg-gray-500'>
+              <Image source={require('./assets/snow.png')} className='h-11 w-11' />
+              <Text className='text-white'>Wednesday</Text>
+              <Text className='text-white text-xl font-semibold'>13&#176;</Text>
+            </View>
+
+            <View className='flex justify-center items-center w-24 rounded-3xl py-3 space-y-1 mr-4 bg-gray-500'>
+              <Image source={require('./assets/drizzle.png')} className='h-11 w-11' />
+              <Text className='text-white'>Thursday</Text>
+              <Text className='text-white text-xl font-semibold'>13&#176;</Text>
+            </View>
+
+            <View className='flex justify-center items-center w-24 rounded-3xl py-3 space-y-1 mr-4 bg-gray-500'>
+              <Image source={require('./assets/cloud.png')} className='h-11 w-11' />
+              <Text className='text-white'>Friday</Text>
+              <Text className='text-white text-xl font-semibold'>13&#176;</Text>
+            </View>
+
+            <View className='flex justify-center items-center w-24 rounded-3xl py-3 space-y-1 mr-4 bg-gray-500'>
+              <Image source={require('./assets/rain.png')} className='h-11 w-11' />
+              <Text className='text-white'>Saturday</Text>
+              <Text className='text-white text-xl font-semibold'>13&#176;</Text>
+            </View>
+
+            <View className='flex justify-center items-center w-24 rounded-3xl py-3 space-y-1 mr-4 bg-gray-500'>
+              <Image source={require('./assets/snow.png')} className='h-11 w-11' />
+              <Text className='text-white'>Sunday</Text>
+              <Text className='text-white text-xl font-semibold'>13&#176;</Text>
+            </View>
+          </ScrollView>
         </View>
       </SafeAreaView>
     </View>
---
+++ constants/index.js
@@ -0,0 +1,9 @@
+export const apiKey = "3e5164bc258a4209b44150342241411";
+
+export const weatherImages = {
+    'Clear': require('../assets/clear.png'),
+    'Cloud': require('../assets/cloud.png'),
+    'Drizzle': require('../assets/drizzle.png'),
+    'Rain': require('../assets/rain.png'),
+    'Snow': require('../assets/snow.png')
+}
